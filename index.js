8e9487370c5a13acb999fa9d691f37056f65db80
boxmein <wsdf294@gmail.com>
2014-08-12 17:01:53 +0300

    errors are now transparent to the user, and err.jade is unused

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// The entry point to the web-app.
 var express = require('express')
   , http    = require('http')
   , path    = require('path')
@@ -16,18 +17,21 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname,'views'));
 app.engine('jade', require('jade').__express);
 
-// POST formats
+// POST data
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(bodyparser.json());
+
+// Logging
 app.use(logfmt.requestLogger());
 
-// Static site data
+// Static stuff like stylesheets
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Index page!
 app.get('/', function(req, res) {
   return res.render('index.jade');
 });
 
+// We got linked to!
 app.get('/l/:link', function(req, res) {
   var rc = new Recaptcha(private_data.recaptcha_public_key,
                          private_data.recaptcha_private_key);
@@ -72,23 +76,26 @@ app.post('/l/:link', function(req, res) {
 
 });
 
+// Create your own link
 app.get('/make', function(req, res) {
   var rc = new Recaptcha(private_data.recaptcha_public_key, 
                          private_data.recaptcha_private_key);
   res.render('make.jade', {  
-    'captcha': rc.toHTML() 
+    'captcha': rc.toHTML(),
+    'err': req.query.err || false
   });
 });
 
 app.post('/make', function(req, res) {
   
   if (!req.body || 
-      !req.body.link ||
-      !req.body.recaptcha_challenge_field ||
+      !req.body.link) {
+    res.redirect('/make?err=empty');
+  }
+
+  if (!req.body.recaptcha_challenge_field || 
       !req.body.recaptcha_response_field) {
-    res.render('err.jade', {
-      'errname': 'no form data sent!'
-    });
+    res.redirect('/make?err=captcha_empty');
   }
 
   var data = {
@@ -102,22 +109,20 @@ app.post('/make', function(req, res) {
                          private_data.recaptcha_private_key, data);
 
   rc.verify(function(success, error_code) {
+    // if the CAPTCHA went okay, work the encrypting part and send the link
+    // to the user.
+
+    // Otherwise, ask them for the link and captcha again!
+
     if (success) {
       var link = req.body.link; 
-      console.log('got link', link);
-    
       var encrypted = bf.encrypt(link);
-      console.log('encrypted link', encrypted);
-      
+
       res.render('make-result.jade', {
         'encrypted': encrypted
       });
     } else {
-      var rc = new Recaptcha(private_data.recaptcha_public_key, 
-                         private_data.recaptcha_private_key);
-      res.render('make.jade', {  
-        'captcha': rc.toHTML() 
-      });
+      res.redirect('/make?err=captcha_fail');
     }
   });
 })
